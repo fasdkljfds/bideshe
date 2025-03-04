0b83043ed8b3dc057dca0bf12bb4e6e87e96eb56
Eric <ericzs2017@outlook.com>
2025-03-04 16:49:19 +0800

    Initial commit

---
+++ EasyEdit/examples/run_wise_editing.sh
@@ -0,0 +1,25 @@
+CUDA_VISIBLE_DEVICES=0 python run_wise_editing.py \
+  --editing_method=WISE \
+  --hparams_dir=../hparams/WISE/llama-3-8b.yaml \
+  --data_dir=../data/wise \
+  --ds_size=10 \
+  --data_type=ZsRE \
+  --sequential_edit
+
+
+#CUDA_VISIBLE_DEVICES=0 python wise_llama3.2-1B_ZsRE.py \
+#   --editing_method=WISE \
+#   --hparams_dir=../hparams/WISE/llama-7b \
+#   --data_dir=../data/wise \
+#   --ds_size=3 \
+#   --data_type=temporal \
+#   --sequential_edit
+
+
+#python wise_llama3.2-1B_ZsRE.py \
+#  --editing_method=WISE \
+#  --hparams_dir=../hparams/WISE/llama-7b \
+#  --data_dir=../data/wise \
+#  --ds_size=3 \
+#  --data_type=hallucination
+##  --sequential_edit
